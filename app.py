a9fef1c6f6d1120e328ac13f75627aa404f03144
Rodrigo Puma <99513585+rodrigopuma@users.noreply.github.com>
2025-07-13 11:45:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,42 @@
 # Estrutura de dados
 
-nome = ''
-descricao = ''
-prioridade = int()
-categoria = ''
-status = ''
-
 def criar_tarefa():
     nome = input('\nDigite o nome: ')
     descricao = input('\nDigite a descrição: ')
-    
+    prioridade = input('\nDigite a prioridade: ')
     categoria = input('\nSelecione a categoria: ') # Quero fazer uma listinha de categorias
-    status = input('\n[ Concluido | Fazer ]\nStatus: ')
+    concluido = False
     tarefa = {
         'Nome': nome,
         'Descrição Tarefa': descricao,
         'Categoria': categoria,
-        'Status': status
+        'Prioridade': prioridade,
+        'Status Concluido': concluido 
     }
     return tarefa
 
-def adicionar_tarefa_a_lista(tarefa, lista):
-    lista.append(tarefa)
-    salvar_tarefa(tarefa)
+# def adicionar_tarefa_a_lista(tarefa):
+#     salvar_tarefa(tarefa)
 
-def listar_tarefas(lista_tarefas):
-    lista_tarefas = []
-    with open("lista_tarefas.txt", "r") as arquivo: # "r" = read (ler os usuarios)
-        for tarefa in arquivo:
-            tarefa.strip().split(';')
-            
- 
-    for tarefa in lista_tarefas:
-        contador += 1
-        print(f'Tarefa {contador}:\n{tarefa}')
+# def listar_tarefas():
+#     contador = 0
+#     with open("lista_tarefas.txt", "r") as arquivo: # "r" = read (ler os usuarios)
+#         for tarefa in arquivo:
+#             tarefa.strip().split(',')
+#             contador += 1
+#             print(f'Tarefa {contador}:\n{tarefa}')
 
-def salvar_tarefa(tarefa):
-    with open("lista_tarefas.txt", "a") as arquivo: # "a" = append (adicionar ao final)
-        arquivo.write(tarefa)
+# def salvar_tarefa(tarefa):
+#     with open("lista_tarefas.txt", "a") as arquivo: # "a" = append (adicionar ao final)
+#         arquivo.write(str(tarefa))
     
-def marcar_tarefa_concluida(lista_tarefas, tarefa):
-    for trf in lista_tarefas:
-        if trf['Nome'] == tarefa['Nome']:
-            tarefa['Status'] = 'Concluido'
-            break
-    return tarefa
+# def marcar_tarefa_concluida(lista_tarefas, tarefa):
+#     for trf in lista_tarefas:
+#         if trf['Nome'] == tarefa['Nome']:
+#             tarefa['Status'] = 'Concluido'
+#             break
+#     return tarefa
+
+# tarefa = criar_tarefa()
+# adicionar_tarefa_a_lista(tarefa)
+# listar_tarefas()
